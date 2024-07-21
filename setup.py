4f88fe35d3909a5d6a5885d46e922486ecb07c51
PatchesTheSnake <drboccabella@gmail.com>
2024-07-21 12:24:50 -0500

    edited

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='adic python',
     packages=find_packages(include=['adic']),
     version='0.1.0',
-    description="""provides support of the p-adicts in python""",
+    description="""provides support of the p-adicts numbers in python""",
     author='Dominic Boccabella',
     install_requires=[],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
-    test_suite='tests',
+    test_suite='/tests',
 )
